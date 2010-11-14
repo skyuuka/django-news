784af860e46065e9de7b9340e5abb958aad35202
Amanjeev <amanjsingh@gmail.com>
2010-11-14 14:47:41 -0500

    commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.commiting before getting more insight in this datetime thing.

--- orch_news/news/views.py
+++ orch_news/news/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from news.models import News
+import datetime
 
 def all_news(request):
-	news_show = News.objects.all()
-	return render_to_response('news.html', {'news_show':news_show})
+	nows = datetime.datetime.now()
+	news_show = News.objects.order_by('-order')
+	return render_to_response('news.html', {'news_show':news_show, 'nows':nows})
