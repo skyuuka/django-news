9e1b0f4cb882c65cafe90e61bd8f9faac22fb5c6
Amanjeev <amanjsingh@gmail.com>
2010-11-14 03:04:51 -0500

    modified the models accordingly.

--- orch_news/news/models.py
+++ orch_news/news/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # New Model (No pun intended)
 class News(models.Model):
-	title = models.TextField()
+	title = models.TextField(blank=True)
 	body = models.TextField()
-	url = models.TextField(blank=True)
+	#url = models.TextField(blank=True)
+	publish_date = models.DateTimeField(blank=True, null=True)
+	unpublish_date = models.DateTimeField(blank=True, null=True)
+	order = models.IntegerField(unique=True)
 	create_date = models.DateTimeField(auto_now_add=True)
 	modified_date = models.DateTimeField(auto_now=True)
-	publish_date = models.DateTimeField()
-	unpublish_date = models.DateTimeField()
