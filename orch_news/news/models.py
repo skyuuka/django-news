91d75fdbc87a804bb4e407d5855126b1d266799d
Amanjeev <amanjsingh@gmail.com>
2010-11-13 23:00:00 -0500

    added admin.py

--- orch_news/news/models.py
+++ orch_news/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# New Model (No pun intended)
+class News(models.Model):
+	title = models.TextField()
+	body = models.TextField()
+	url = models.TextField()
+	create_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+	publish_date = models.DateTimeField()
+	unpublish_date = models.DateTimeField()
