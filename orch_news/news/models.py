e7569ff0e8d8fad240fc87d7259b08cfee487ca9
Amanjeev <amanjsingh@gmail.com>
2010-11-14 01:07:49 -0500

    views.py edited and started working on views now.

--- orch_news/news/models.py
+++ orch_news/news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class News(models.Model):
 	title = models.TextField()
 	body = models.TextField()
-	url = models.TextField()
+	url = models.TextField(blank=True)
 	create_date = models.DateTimeField(auto_now_add=True)
 	modified_date = models.DateTimeField(auto_now=True)
 	publish_date = models.DateTimeField()
--- orch_news/urls.py
+++ orch_news/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import *
+from news.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^orch_news/', include('orch_news.foo.urls')),
+    # all news:
+    (r'^news/$', all_news),
+
+	# single news stories
+	
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
