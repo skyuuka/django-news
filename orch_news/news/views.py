c753e6f13e9ee6b7d6fd63090480a70d79e8eba7
Amanjeev <amanjsingh@gmail.com>
2010-11-14 15:24:38 -0500

    before sending for some review

--- orch_news/news/views.py
+++ orch_news/news/views.py
@@ -6,4 +6,9 @@ import datetime
 def all_news(request):
 	nows = datetime.datetime.now()
 	news_show = News.objects.order_by('-order')
+	"""
+	news_show = News.objects.filter(
+		publish_date = 
+	)
+	"""
 	return render_to_response('news.html', {'news_show':news_show, 'nows':nows})
