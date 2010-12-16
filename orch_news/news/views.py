4ea19ec871e61d43d636d0df917d9e7829bba42c
Amanjeev <amanjsingh@gmail.com>
2010-12-15 21:38:50 -0500

    ommitted the wrong files. now correct.

--- orch_news/news/views.py
+++ orch_news/news/views.py
@@ -5,6 +5,6 @@ import datetime
 
 def all_news(request):
 	nows = datetime.datetime.now()
-	news_show = News.objects.all().order_by('-order') #All
-	#news_show = News.objects.exclude(unpublish_date__lte=nows).exclude(publish_date__gte=nows).order_by('-order')
+	#news_show = News.objects.all().order_by('-order') #All
+	news_show = News.objects.exclude(unpublish_date__lte=nows).exclude(publish_date__gte=nows).order_by('-order')
 	return render_to_response('news.html', {'news_show':news_show, 'nows':nows})
