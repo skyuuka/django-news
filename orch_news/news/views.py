ec9967a3d726829f317dd47bbd9428c08db9af5c
Amanjeev <amanjsingh@gmail.com>
2010-12-15 21:23:00 -0500

    Works! Just some more testing and then final commit

--- orch_news/news/views.py
+++ orch_news/news/views.py
@@ -5,10 +5,6 @@ import datetime
 
 def all_news(request):
 	nows = datetime.datetime.now()
-	news_show = News.objects.exclude(publish_date__lte=nows, unpublish_date__lte=nows).exclude(publish_date__gte=nows, unpublish_date__gte=nows).order_by('-order')
-	"""
-	news_show = News.objects.filter(
-		publish_date = 
-	)
-	"""
+	news_show = News.objects.all().order_by('-order') #All
+	#news_show = News.objects.exclude(unpublish_date__lte=nows).exclude(publish_date__gte=nows).order_by('-order')
 	return render_to_response('news.html', {'news_show':news_show, 'nows':nows})
